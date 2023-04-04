669e000f85d36f55322dc86be63a0d5924757700
Carmela Colqui <carmela.colqui@gmail.com>
2023-04-04 04:58:17 -0300

    Trabajo Practico 7

--- build/obj/externals/base/cpu/arm-thumb/sys_newlib/src/system.d
+++ build/obj/externals/base/cpu/arm-thumb/sys_newlib/src/system.d
@@ -1,2 +1,2 @@
 build/obj/externals/base/cpu/arm-thumb/sys_newlib/src/system.o: \
- /home/pablo/muju/externals/base/cpu/arm-thumb/sys_newlib/src/system.c
+ /home/carmelac/muju/externals/base/cpu/arm-thumb/sys_newlib/src/system.c
--- build/obj/externals/base/soc/lpc43xx/lpc_startup/src/vendor_interrupt.d
+++ build/obj/externals/base/soc/lpc43xx/lpc_startup/src/vendor_interrupt.d
@@ -1,2 +1,2 @@
 build/obj/externals/base/soc/lpc43xx/lpc_startup/src/vendor_interrupt.o: \
- /home/pablo/muju/externals/base/soc/lpc43xx/lpc_startup/src/vendor_interrupt.c
+ /home/carmelac/muju/externals/base/soc/lpc43xx/lpc_startup/src/vendor_interrupt.c
